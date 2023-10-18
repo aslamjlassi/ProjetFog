7474f48d54a84b3d3e2381d268145e9feff3b942
aslamjlassi <aslamjlassi12345@gmail.com>
2023-10-18 12:42:45 +0100

    Replaced HTTP with HTPPS for more security

--- MainServer.js
+++ MainServer.js
@@ -1,31 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3002;
-const axios = require('axios');
-
-const serverBURL = 'http://192.168.1.153:3003'; // Replace with the IP address of Server A
-const serverAURL = 'http://192.168.1.175:3001'; // Replace with the IP address of Server B
-
-app.use(express.json({ limit: '10mb' }));
-
-app.post('/process-image', async (req, res) => {
+const https = require("https");
+const axios = require("axios");
+const serverBURL = "https://<ip_ address >:3003"; // Replace with the IP address of Server A
+const serverAURL = "https:// <ip_ address >:3001"; // Replace with the IP address of Server B
+app.use(express.json({ limit: "10mb" }));
+const sslOptions = {
+  cert: fs.readFileSync("path/cert.pem"),
+  key: fs.readFileSync("path/key.pem"),
+}; // Créez un serveur HTTPS avec Express
+const httpsServer = https.createServer(sslOptions, app);
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+});
+app.post("/process-image", async (req, res) => {
   try {
     const image = req.body.imageData;
-
-    
-
     const selectedServerURL = chooseServerURL(); // Implement your server selection logic
-
-    const result = await axios.post(selectedServerURL, { imageData: image });
-
-    res.send( result.data );
+    const result = await axiosInstance.post(selectedServerURL, {
+      imageData: image,
+    });
+    res.send(result.data);
   } catch (error) {
-    console.error('Error processing image:', error);
-    res.status(500).send('Image processing failed');
+    console.error("Error processing image:", error);
+    res.status(500).send("Image processing failed");
   }
 });
-
-app.listen(port, () => {
+httpsServer.listen(port, () => {
   console.log(`Main server is running on port ${port}`);
 });
 
@@ -40,5 +42,5 @@ function chooseServerURL() {
   // Update the currentServerIndex for the next request
   currentServerIndex = (currentServerIndex + 1) % servers.length;
 
-  return selectedServerURL+'/process-image';
+  return selectedServerURL + "/process-image";
 }
--- serverB.js
+++ serverB.js
@@ -1,52 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const fs = require('fs');
-const path = require('path');
-const { spawn } = require('child_process');
-
-app.use(express.json({ limit: '10mb' }));
-
-app.post('/process-image', async (req, res) => {
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const { spawn } = require("child_process");
+app.use(express.json({ limit: "10mb" }));
+const sslOptions = {
+  cert: fs.readFileSync("cert.pem"),
+  key: fs.readFileSync("key.pem"),
+}; // Créez un serveur HTTPS avec Express
+const httpsServer = https.createServer(sslOptions, app);
+app.post("/process-image", async (req, res) => {
   try {
     if (!req.body.imageData) {
-      return res.status(400).send('Image data is required');
+      return res.status(400).send("Image data is required");
     }
-
     // Save the incoming image (before processing) as a reference
-    fs.writeFileSync('input_image.jpg', Buffer.from(req.body.imageData, 'base64'));
-
-    // Execute the Python script for image segmentation (GrabCut)
-    const pythonScriptPath = path.join(__dirname, 'ObjectDetection.py');
-    const inputImagePath = path.join(__dirname, 'input_image.jpg');
-    const outputImagePath = path.join(__dirname, 'output_image.jpg');
-
-    const pythonProcess = spawn('python', [pythonScriptPath, inputImagePath, outputImagePath]);
-
-    pythonProcess.stdout.on('data', (data) => {
+    fs.writeFileSync(
+      "input_image.jpg",
+      Buffer.from(req.body.imageData, "base64")
+    ); // Execute the Python script for image segmentation (GrabCut)
+    const pythonScriptPath = path.join(__dirname, "ObjectDetection.py");
+    const inputImagePath = path.join(__dirname, "input_image.jpeg");
+    const outputImagePath = path.join(__dirname, "output_image.jpeg");
+    const pythonProcess = spawn("python", [
+      pythonScriptPath,
+      inputImagePath,
+      outputImagePath,
+    ]);
+    pythonProcess.stdout.on("data", (data) => {
       console.log(`Python stdout: ${data}`);
     });
-
-    pythonProcess.stderr.on('data', (data) => {
+    pythonProcess.stderr.on("data", (data) => {
       console.error(`Python stderr: ${data}`);
     });
-
-    pythonProcess.on('close', (code) => {
+    pythonProcess.on("close", (code) => {
       console.log(`Python process exited with code ${code}`);
       if (code === 0) {
         // Read the segmented image and send it as a response
-        const processedImageData = fs.readFileSync(outputImagePath, { encoding: 'base64' });
+        const processedImageData = fs.readFileSync(outputImagePath, {
+          encoding: "base64",
+        });
         res.send({ processedImage: processedImageData });
       } else {
-        res.status(500).send('Object Detection failed');
+        res.status(500).send("Object Detection failed");
       }
     });
   } catch (error) {
-    console.error('Error processing image:', error);
-    res.status(500).send('Image processing failed');
+    console.error("Error processing image:", error);
+    res.status(500).send("Image processing failed");
   }
 });
-
-app.listen(port, () => {
-  console.log(`Server B is running on port ${port}`);
+httpsServer.listen(port, () => {
+  console.log(`Server A is running on port ${port}`);
 });
